69334fcc2114742b37abbcb25429874896455a91
Jack Gilcrest <me@jp4g.llc>
2023-02-07 17:02:11 -0500

    commit progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,22 @@
-FROM node:16-buster
+# Build stage for rapidsnark
+FROM node:16-buster-slim as rapidsnark-builder
+
+# update global dependencies & add rapidsnark build dependencies
+RUN apt-get update && apt-get install git curl build-essential libgmp-dev libsodium-dev nasm -y
+
+# Build iden3/rapidsnark source
+RUN git clone https://github.com/iden3/rapidsnark.git && \
+    cd rapidsnark && \
+    git submodule init && \
+    git submodule update && \
+    npm install && \
+    npx task createFieldSources && \
+    npx task buildProver
+
+# Build stage for canon unirep app
+FROM node:16-buster as canon-builder
+
+RUN apt-get update && apt-get install curl build-essential libgmp-dev libsodium-dev nasm -y
 
 COPY . /src
 
@@ -14,9 +32,12 @@ COPY packages/contracts/node_modules/@unirep /src/packages/contracts/node_module
 RUN rm -rf /src/packages/relay/node_modules/@unirep/circuits/zksnarkBuild
 RUN rm -rf /src/packages/contracts/node_modules/@unirep/circuits/zksnarkBuild
 
-FROM node:16-buster
+FROM node:16-buster-slim as daemon
+RUN apt-get update && apt-get install curl build-essential libgmp-dev libsodium-dev nasm -y
 
-COPY --from=0 /src /src
+COPY --from=canon-builder /src /src
+COPY --from=rapidsnark-builder /rapidsnark/build/prover /usr/local/bin
+
 WORKDIR /src/packages/relay
 
 CMD ["npm", "start"]
--- packages/relay/src/singletons/prover.mjs
+++ packages/relay/src/singletons/prover.mjs
@@ -2,11 +2,72 @@ import path from 'path'
 import * as snarkjs from 'snarkjs'
 import fs from 'fs'
 import url from 'url'
+import { exec } from 'child_process'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 const keyPath = path.join(__dirname, '../../keys')
 
+
 export default {
+
+  // /**
+  //  * Write input.json to /tmp/
+  //  * @notice fed into rapidnsnark
+  //  * @param inputs - json object of properly named key value pairs mapping to circuit inputs
+  //  */
+  // writeInputs: (
+  //   inputs
+  // ) => {
+
+  // }
+
+  /**
+   * Generate the witness for a given input for a circuit and save to fs in /tmp/
+   * 
+   * @param {string} circuitName - name of zk circuit being used
+   * @param {Object} inputs - json object of properly named key value pairs mapping to circuit inputs
+   */
+  genWitnessFs: async (
+    circuitName,
+    inputs
+  ) => {
+    // define path to circuit wasm artifact
+    const circuitWasmPath = path.join(
+      keyPath,
+      `${circuitName}.wasm`
+    )
+    // define path to export witness file to
+    const witnessPath = path.join('tmp', `${circuitName}.wtns`)
+    // generate witness and write to /tmp/{CIRCUIT_NAME}.wtns
+    await snarkjs.groth16.fullProvingKeyGen(circuitWasmPath, witnessPath, inputs)
+  },
+
+  genProofAndPublicSignals: async (
+    circuitName,
+    inputs
+  ) => {
+    // write inputs.json to /tmp/inputs.json
+    fs.writeFileSync('/tmp/inputs.json', JSON.stringify(inputs));
+    // write witness to /tmp/{CIRCUIT_NAME}.wtns
+    await this.genWitnessFs(circuitName, inputs);
+    // define path to proving artifact inputs / outputs
+    const zkeyPath = path.join(keyPath, `${circuitName}.zkey`)
+    const witnessPath = path.join("tmp", `${circuitName}.witness`)
+    const proofPath = path.join("tmp", `${circuitName}-proof.json`)
+    const publicSignalsPath = path.join("tmp", `${circuitName}-signals.json`)
+    // spawn child_process to build proof and public signals using rapidsnark
+    exec(`rapidsnark ${zkeyPath} ${witnessPath} ${proofPath} ${publicSignalsPath}`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      return {
+        proof: require(proofPath),
+        publicSignals: require(publicSignalsPath)
+      }
+    });
+  },
+
   genProofAndPublicSignals: async (
     circuitName,
     inputs
