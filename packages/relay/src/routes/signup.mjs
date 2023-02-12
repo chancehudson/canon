e0d81a8d0d46f2ed73bdc1e52e2656339f92dc67
Jack Gilcrest <me@jp4g.llc>
2023-02-11 20:06:38 -0500

    key server files set up + does proof does not verify?

--- packages/relay/src/routes/signup.mjs
+++ packages/relay/src/routes/signup.mjs
@@ -17,30 +17,42 @@ export default ({ app, db, synchronizer }) => {
     try {
       const { publicSignals, proof } = req.body
       const signupProof = new SignupProof(publicSignals, proof, synchronizer.prover)
+      console.log('signupProof', signupProof);
       const valid = await signupProof.verify()
+      console.log('valid proof');
       if (!valid) {
+        console.log('/api/signup[400]: invalid signup proof');
         res.status(400).json({ error: 'Invalid proof' })
         return
       }
+      console.log('getting epoch...')
       const currentEpoch = synchronizer.calcCurrentEpoch()
+      console.log('got epoch', currentEpoch)
       if (currentEpoch !== Number(BigInt(signupProof.epoch))) {
+        console.log('/api/signup[400]: invalid signup epoch');
         res.status(400).json({ error: 'Wrong epoch' })
         return
       }
+      console.log('making tx')
       // make a transaction lil bish
       const canonContract = new ethers.Contract(CANON_ADDRESS, CanonABI)
+      console.log('contract', canonContract)
       // const contract =
       const calldata = canonContract.interface.encodeFunctionData(
         'signup',
         [signupProof.publicSignals, signupProof.proof]
       )
+      console.log('calldata: ', calldata);
       const hash = await TransactionManager.queueTransaction(
         CANON_ADDRESS,
         calldata,
       )
+      console.log('hash:', hash);
+
       res.json({ hash })
 
     } catch (error) {
+      console.log('error: ', error)
       res.status(500).json({ error })
     }
 
