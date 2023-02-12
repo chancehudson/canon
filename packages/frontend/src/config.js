fd0f4fdabcb1ea00bbc1eacc4d2d96a0938bdea7
Chance <jchancehud@gmail.com>
2023-02-11 20:19:23 -0600

    fix: correct keys

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -1,11 +1,12 @@
 import { ethers } from 'ethers'
-export const UNIREP_ADDRESS = '0xB69426AC9537623B7c69622b8dfF6058D9D78b6c'
-export const CANON_ADDRESS = '0xcF9e9683734926846CfE4875Ce09E498df7FF9a7'
+export const UNIREP_ADDRESS = '0x24b540A1E487fdb0a30140Ad3ADe82CCa5F4e1F4'
+export const CANON_ADDRESS = '0x8EB848cc903383986153711eEBdd81CA7d88856B'
 export const ETH_PROVIDER_URL = 'https://arbitrum.goerli.unirep.io'
 // export const ETH_PROVIDER_URL = 'http://localhost:8545'
 
 export const provider = ETH_PROVIDER_URL.startsWith('http') ? new ethers.providers.JsonRpcProvider(ETH_PROVIDER_URL) : new ethers.providers.WebSocketProvider(ETH_PROVIDER_URL)
 
-export const KEY_SERVER = 'https://keys.unirep.io/2-alpha-2/'
+// export const KEY_SERVER = 'https://keys.unirep.io/2-alpha-2/'
+export const KEY_SERVER = 'http://localhost:8000/build/'
 // export const SERVER = 'https://canon.tubby.cloud'
 export const SERVER = 'http://localhost:8000'
--- packages/relay/src/config.mjs
+++ packages/relay/src/config.mjs
@@ -2,8 +2,8 @@ import { ethers } from 'ethers'
 import { config } from 'dotenv'
 config()
 
-export const UNIREP_ADDRESS = process.env.UNIREP_ADDRESS ?? '0xB69426AC9537623B7c69622b8dfF6058D9D78b6c'
-export const CANON_ADDRESS = process.env.CANON_ADDRESS ?? '0xcF9e9683734926846CfE4875Ce09E498df7FF9a7'
+export const UNIREP_ADDRESS = process.env.UNIREP_ADDRESS ?? '0x24b540A1E487fdb0a30140Ad3ADe82CCa5F4e1F4'
+export const CANON_ADDRESS = process.env.CANON_ADDRESS ?? '0x8EB848cc903383986153711eEBdd81CA7d88856B'
 export const ETH_PROVIDER_URL = process.env.ETH_PROVIDER_URL ?? 'https://arbitrum.goerli.unirep.io'
 // export const ETH_PROVIDER_URL = process.env.ETH_PROVIDER_URL ?? 'http://localhost:8545'
 // export const PRIVATE_KEY = process.env.PRIVATE_KEY ?? '0x4fba49cf81efd3609744b42bfaafbd1fa55b381ee9de06045677842cf38ee940'
